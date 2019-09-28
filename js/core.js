158fb08def661559c7c665a8a15cbe1951a6f0ac
Darydaa <foxing18lis@gmail.com>
2019-09-28 23:15:05 +0300

    Исправлены тайминги и размеры, добавлена маска

---
+++ js/core.js
@@ -0,0 +1,5 @@
+var closedStand=document.getElementsByClassName('closedStand')[0];
+var openedStand=document.getElementsByClassName('openedStand')[0];
+setTimeout(function(){openedStand.style.display="unset";},3200);
+setTimeout(function(){closedStand.style.display="none";},3200);
+
