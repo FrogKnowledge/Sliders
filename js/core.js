5701509263cb470ec9d8db01f739256f10dc1050
frog <memopad258@gmail.com>
2019-09-29 18:52:18 +0300

    Работа над 2м слайдом

--- js/core.js
+++ js/core.js
@@ -1,5 +1,10 @@
 var closedStand=document.getElementsByClassName('closedStand')[0];
 var openedStand=document.getElementsByClassName('openedStand')[0];
-setTimeout(function(){openedStand.style.display="unset";},3200);
-setTimeout(function(){closedStand.style.display="none";},3200);
+var openedStandOp=document.getElementsByClassName('openedStandOp')[0];
+setTimeout(function(){openedStand.style.visibility="visible";},3200);
+setTimeout(function(){closedStand.style.visibility="hidden";},3200);
+setTimeout(function(){openedStandOp.style.visibility="visible";},5800);
+setTimeout(function(){openedStand.style.visibility="hidden";},5800);
+
+
 
